3ccf466f806d53891c23e2b2c4c03ee1e4108dbe
Alexandre Moretti <alemoretti10@gmail.com>
2025-11-24 19:04:00 -0300

    Configure scheduled task for automatic SWAPI data sync

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,18 @@
 <?php
 
+use App\Jobs\SyncAllSwapiDataJob;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule SWAPI data synchronization
+Schedule::job(new SyncAllSwapiDataJob())
+    ->dailyAt('02:00')
+    ->withoutOverlapping()
+    ->onOneServer()
+    ->name('swapi-sync')
+    ->description('Synchronize Star Wars data from SWAPI');
